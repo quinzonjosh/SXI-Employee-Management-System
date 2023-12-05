3641fcabb67b2a2cf4f430eadcb9ac1ab81bf8b2
quinzonjosh <quinzonjosh261@gmail.com>
2023-12-05 12:37:53 +0800

    implemented update employee frontend

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import AddEmployee from "./Employee/AddEmployee";
 import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditEmployee from "./Employee/EditEmployee";
 
 export function App() {
   return (
@@ -9,8 +10,9 @@ export function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path ="/" element={<Home/>}/>
+          <Route exact path ="/employees" element={<Home/>}/>
           <Route exact path ="/addEmployee" element={<AddEmployee/>}/>
+          <Route exact path ="/editEmployee/:employeeID" element={<EditEmployee/>}/>
         </Routes>
       </Router>        
     </>
