ab079c921a8fa8f418ca065f1902aa7a376eadf9
Josh Quinzon <quinzonjosh261@gmail.com>
2024-02-13 17:43:49 +0800

    implemented search by last name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+// LAST STOP: update search results on backspace key
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   const [employees, setEmployees] = useState([]);
+  // extra copy of the employees to be able to filter search properly
+  const [ employeesCopy, setEmployeesCopy ] = useState([]);
 
   useEffect(() => {
     loadEmployees();
@@ -12,19 +15,47 @@ const Home = () => {
   const loadEmployees = async () => {
     const result = await axios.get("http://localhost:8080/currentEmployees");
     setEmployees(result.data);
+    setEmployeesCopy(result.data);
     // console.log(result.data);
   };
 
   const handleDelete = async (employeeID) => {
     // console.log(employeeID);
-    const res = await axios.delete(`http://localhost:8080/employees/softDelete/${employeeID}`);
+    const res = await axios.delete(
+      `http://localhost:8080/employees/softDelete/${employeeID}`
+    );
     // console.log(res.data);
     loadEmployees();
-  }
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    if(value === ""){
+      setEmployees(employeesCopy);
+    } else {
+      console.log(value);
+      const filteredEmployees = employeesCopy.filter((employee)=>
+        employee.lastName.toLowerCase().startsWith(value.toLowerCase())
+      );
+      // console.log(filteredEmployees);
+      setEmployees(filteredEmployees);
+    }
+
+  };
 
   return (
-    <div className="">
+    <div className="container">
       <div className="p-4">
+        <div class="d-flex justify-content-center">
+          <input
+            type="text"
+            className="w-50 mb-4"
+            placeholder="Search an employee"
+            name="searchEmployee"
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
         <table className="table border shadow">
           <thead>
             <tr>
@@ -36,21 +67,36 @@ const Home = () => {
             </tr>
           </thead>
           <tbody>
-            {
-                employees.map((employee, index)=>(
-                    <tr>
-                        <th scope="row" key={index}>{employee.employeeID}</th>
-                        <td>{employee.companyID}</td>
-                        <td>{employee.firstName}</td>
-                        <td>{employee.lastName}</td>
-                        <td>
-                            <Link to={`/employees/${employee.employeeID}`} className="btn btn-info mx-2">View</Link>
-                            <Link to={`/editEmployee/${employee.employeeID}`} className="btn btn-dark mx-2">Edit</Link>
-                            <button onClick={() => handleDelete(employee.employeeID)} className="btn btn-danger mx-2">Delete</button>
-                        </td>
-                    </tr>
-                ))
-            }
+            {employees.map((employee, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {employee.employeeID}
+                </th>
+                <td>{employee.companyID}</td>
+                <td>{employee.firstName}</td>
+                <td>{employee.lastName}</td>
+                <td>
+                  <Link
+                    to={`/employees/${employee.employeeID}`}
+                    className="btn btn-info mx-2"
+                  >
+                    View
+                  </Link>
+                  <Link
+                    to={`/editEmployee/${employee.employeeID}`}
+                    className="btn btn-dark mx-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => handleDelete(employee.employeeID)}
+                    className="btn btn-danger mx-2"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
